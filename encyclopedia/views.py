2beb15bed48d38db8fea518defc8348adc68c8f7
Egor4ik325 <zorinegor352@gmail.com>
2021-07-04 23:33:55 +0300

    Implement entry representation.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,10 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry_content = util.get_entry(title)
+    if not entry_content:
+        entry_content = "Sorry, this encyclopedia page doesn't exist."
+
+    context = {'title': title, 'entry': entry_content}
+    return render(request, "encyclopedia/entry.html", context)
